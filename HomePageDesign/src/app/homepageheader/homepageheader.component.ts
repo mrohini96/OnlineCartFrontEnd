7bafc20235fa3fdd5c16e601bba92e27b47c2f15
Rohini B Mallewade <Rohini.Mallewade@trianz.com>
2018-07-25 15:20:20 +0530

    "Design added"

--- HomePageDesign/src/app/homepageheader/homepageheader.component.ts
+++ HomePageDesign/src/app/homepageheader/homepageheader.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+/*import { Component, OnInit } from '@angular/core';
 
-//import {UserService} from '../user.service';
+import {UserService} from '../user.service';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs';
+//import { map } from 'rxjs/add/operator/map';
 
 
 
@@ -11,8 +15,14 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
   styleUrls: ['./homepageheader.component.css']
 })
 export class HomepageheaderComponent implements OnInit {
- 
-  constructor () { }
+  private url=" ";
+  data : any= {};
+  constructor (private http :Http, private user:UserService) { 
+    console.log("hello this header's constructor");
+    this.onSubmit();
+    let test = this.user.myApiCall();
+    console.log(test);
+  }
   validate:boolean = false;
   userJson={"name": "", "pswd": "" };
   userList = [
@@ -30,6 +40,66 @@ export class HomepageheaderComponent implements OnInit {
     }
   ];
 
+
+  ngOnInit() {
+ 
+  }
+  onSubmit() {
+   this.userList.forEach((user) => {
+    if (this.userJson.name == user.name && this.userJson.pswd == user.pswd ){
+      this.validate = true;
+    }
+   }) 
+  } 
+ 
+  
+} */
+
+
+import { Component, OnInit } from '@angular/core';
+
+//import {UserService} from '../user.service';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
+
+
+@Component({
+  selector: 'app-homepageheader',
+  templateUrl: './homepageheader.component.html',
+  styleUrls: ['./homepageheader.component.css']
+})
+export class HomepageheaderComponent implements OnInit {
+ 
+  constructor () { }
+  validate:boolean = false;
+  switchPage = "page1";
+  userJson={"name": "", "pswd": "" };
+  userList = [
+    {
+      "name": "Satya",
+      "pswd": "hi"
+    },
+    {
+      "name": "Rohini",
+      "pswd": "12345"
+    },
+    {
+      "name": "Nivedita",
+      "pswd": "1234"
+    }
+  ];
+  
+  moveCart(){
+
+  this.switchPage="page3"
+
+  }
+
+
+  movePage(){
+    this.switchPage = "page2";
+  }
+
   onSubmit() {
    this.userList.forEach((user) => {
     if (this.userJson.name == user.name && this.userJson.pswd == user.pswd ){
@@ -38,7 +108,7 @@ export class HomepageheaderComponent implements OnInit {
    }) 
     }
  ngOnInit() {
- 
+
   }
   
 }
--- HomePageDesign/src/app/user.service.ts
+++ HomePageDesign/src/app/user.service.ts
@@ -1,20 +1,45 @@
 import { Injectable } from '@angular/core';
 import { Products } from './products';
+//import 'rxjs/add/operator/catch';
 // import { User } from './user';
 import { Http, Response } from '@angular/http';
 //import { map } from 'rxjs/add/operator/map';
+//import 'rxjs/Rx';
 import { Observable } from 'rxjs';
+import { map } from "rxjs/operators"
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
   constructor(private http: Http) { }
-/*
-  myApiCall(){
-    return this.http.get("http://localhost:8080/onlinecart-1.0/onlinecart/loginuserget?name=Rohini@gmail.com&password=1234").map(res => res.json());
+
+  myApiCall(): Observable<any>{
+    return this.http.get(
+      "http://localhost:8080/onlinecart-1.0/onlinecart/loginuserget?name=Rohini@gmail.com&password=1234")
+      .pipe(map(res => res.json()));
+      // .map(res => res.json());
+     // (map((response: any) => response.json()));
+
   }
-  */
+
+
+  /*saveAsupPref(url,params): Observable<any>{
+
+    let _urlParams = new URLSearchParams();
+    for(let element in params) {
+      _urlParams.append(element,params[element]);
+    }
+    
+      return this.http.post( url, _urlParams )
+      .map(this.extractData)
+      .catch(this.handleError);
+  
+  }*/
+
+
+
+
   myData(userObj) {
     if (userObj.name == 'Rohini' && userObj.pswd == '12345' ){
       alert("login successful");
